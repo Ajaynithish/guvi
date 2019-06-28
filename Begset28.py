eeb788ef494d4e3c682bdeb7e6c4ac1c4370b188
Ajaynithish <38007337+Ajaynithish@users.noreply.github.com>
2019-06-28 10:49:39 +0530

    Create Begset28.py

---
+++ Begset28.py
@@ -0,0 +1,10 @@
+r1,r2=map(int,input().split())
+for inp in range(r1+1,r2):
+    if(0<inp<10):
+        print(inp,end=" ")
+    elif(inp==153 or inp==370 or inp==371 or inp==407):
+        print(inp,end=" ")
+    elif(inp==1634 or inp==8203 or inp==9474):
+        print(inp,end=" ")
+    elif(inp==54748 or inp==92727 or inp==93084):
+        print(inp,end=" ")
